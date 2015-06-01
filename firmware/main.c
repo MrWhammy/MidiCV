90767a10dea63743bae45accc812672e77d222ba
Brecht Yperman <brecht@yperman.eu>
2015-06-01 21:01:15 +0200

    Sturen van uC naar host

--- firmware/main.c
+++ firmware/main.c
@@ -25,6 +25,7 @@ Defines
 
 #define USB_LED_OFF 0
 #define USB_LED_ON  1
+#define USB_DATA_OUT 2
  
 /********************************************************************************
 Interrupt Routines
@@ -139,6 +140,9 @@ void handleNote(unsigned char message, unsigned char channel) {
 /********************************************************************************
 USB
 ********************************************************************************/
+
+static uchar replyBuf[16] = "Hello, Brecht!";
+
 // this gets called when custom control message is received
 USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
     usbRequest_t *rq = (void *)data; // cast data to correct type
@@ -150,6 +154,9 @@ USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
     case USB_LED_OFF: 
         PORTB &= ~(_BV(PB2)); // turn LED off
         return 0;
+    case USB_DATA_OUT: // send data to PC
+        usbMsgPtr = replyBuf;
+        return sizeof(replyBuf);
     }
 
     return 0; // should not get here
@@ -163,18 +170,19 @@ int main( void ) {
   // Configure PORTB as output
   DDRB |= _BV(PB2);
 
-	uchar i;
-
     wdt_enable(WDTO_1S); // enable 1s watchdog timer
 
+    // ========= USB INIT =============
     usbInit();
         
     usbDeviceDisconnect(); // enforce re-enumeration
+    uchar i;
     for(i = 0; i<250; i++) { // wait 500 ms
         wdt_reset(); // keep the watchdog happy
         _delay_ms(2);
     }
     usbDeviceConnect();
+    // ==============
         
     sei(); // Enable interrupts after re-enumeration
         
